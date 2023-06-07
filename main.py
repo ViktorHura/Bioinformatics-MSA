e6b1a6f172983e4c212116786eb8b94dc64b4717
yourpingu <viktorhura.info@gmail.com>
2023-06-07 12:56:35 +0200

    Global alignment working

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ from itertools import product, combinations
 MATCH = 5
 MISMATCH = -2
 INDEL = -4
-MULTI_GAP_THRESHOLD = 2
-MULTI_GAP = 0
+GAPGAP = 0
+
+# could modify this to use a substitution matrix
+def replacementScore(A, B):
+    return MATCH if A == B else MISMATCH
+
 
 class NMatrix:
+    # dimension_length in the form of [..., #pages, #rows, #cols]
     def __init__(self, dimension_lengths):
         self.dims = dimension_lengths
 
@@ -15,62 +20,53 @@ class NMatrix:
         for l in dimension_lengths:
             self.total_size *= l
 
+        # each cell in the form of (score, parent coordinate mask)
         self.data = [(None, [])] * self.total_size
 
-    def __keyToIndex__(self, key):
+    # convert n-tuple to index in our 1D array
+    def __tupleToIndex__(self, key):
         index = 0
+        # sum of products, in 3D: index(p,r,c) = (p * #rows * #cols) + (r * #cols) + c
         for i, coord in enumerate(key):
+            # last term is not a product
             if i + 1 == len(self.dims):
                 index += coord
                 continue
             product = coord
+            # product of all subsequent dimension lengths
             for j in range(i + 1, len(self.dims)):
                 product *= self.dims[j]
             index += product
         return index
 
     def __getitem__(self, key):
-        index = self.__keyToIndex__(key)
-        if index < 0 or index >= len(self.data):
-            return None
         for k in key:
             if k < 0:
                 return None
+        index = self.__tupleToIndex__(key)
+        if index < 0 or index >= len(self.data):
+            return None
 
         return self.data[index]
 
     def __setitem__(self, key, value):
-        index = self.__keyToIndex__(key)
+        index = self.__tupleToIndex__(key)
         self.data[index] = value
 
 
-    def __repr__(self):
-        if len(self.dims) == 3:
-            result = ""
-            for p in range(self.dims[0]):
-
-                for r in range(self.dims[1]):
-                    rowStr = ""
-                    for c in range(self.dims[2]):
-                        rowStr += str(self.__getitem__((p, r, c))[0]) + ", "
-                    result += rowStr + '\n'
-
-                result += '\n'
-            return result
-        else:
-            return str(self.data)
-
-
+# relative coordinates of each to consider neighbour in n dimensions
+# will yield 3 neighbours in 2D, 7 in 3D, 2^n - 1 neighbours in N-D
 def neighbourMask(dim_count):
     masks = list(product([0, -1], repeat=dim_count))
     masks.remove(tuple([0]*dim_count))
     return [list(m) for m in masks]
 
 
+# increments the current matrix position in order
 def nextMatrixPosition(current_position, dimensions):
     new_position = current_position
-    i = len(current_position) - 1
-    while(i != -1):
+    i = len(current_position) - 1   # start with last value in tuple
+    while(i != -1):                 # increment from back to front and carry over if needed
         new_position[i] += 1
         if new_position[i] == dimensions[i]:
             new_position[i] = 0
@@ -80,118 +76,149 @@ def nextMatrixPosition(current_position, dimensions):
     return new_position
 
 
+# element wise sum of two positions
 def vectorSum(v1, v2):
     return [v + v2[i] for i, v in enumerate(v1)]
 
 
-def MSA(sequences):
+# returns score derived from a neighbour cell
+# given the current position, sequences, neighbour score and mask for neighbour position
+def parseNeighbour(position, sequences, n_score, n_mask):
+    score = n_score
+
+    pairwise_combos = list(combinations(range(len(n_mask)), 2))
+    for combo in pairwise_combos:
+        aI, bI = combo
+        im = n_mask[aI]
+        jm = n_mask[bI]
+
+        if (im, jm) == (0,0):
+            score += GAPGAP
+        elif (im, jm) == (0, -1) or (im, jm) == (-1, 0):
+            score += INDEL
+        elif (im, jm) == (-1, -1):
+            seqA = sequences[aI]
+            seqB = sequences[bI]
+            A = seqA[position[aI]]
+            B = seqB[position[bI]]
+            score += replacementScore(A,B)
+    return score
+
+
+# get global alignment path in a filled NMatrix
+# path in the form of [ (parent mask, cell position) ,...]
+def globalAlignmentPath(matrix):
+    path = []
+    cur_pos = [d-1 for d in matrix.dims]        # start from the end
+    origin = [0] * len(matrix.dims)             # stop when back at origin
+    while cur_pos != origin:
+        cell = matrix[cur_pos]
+        path.append((cell[1], cur_pos))         # (parent mask, cell position)
+        cur_pos = vectorSum(cur_pos, cell[1])   # next position = parent position (via mask)
+    path.reverse()
+    return path
+
+
+def parseGlobalAlignments(sequences, path):
+    alignments = ["" for s in sequences]    # store alignments as list of strings
+    maxlen = 0                              # longest alignment length, to pad out the other ones
+
+    for node in path:
+        delta = node[0]
+        for pos, coord in enumerate(delta):
+            sequence = sequences[pos]
+            letter_index = node[1][pos]
+            letter_to_consume = sequence[letter_index]
+            alignments[pos] += "." if coord == 0 else letter_to_consume
+
+    for i, a in enumerate(alignments):
+        # take sequence and replace each letter that is consumed already in alignment
+        # this gives us the leftover letters
+        residue = sequences[i][1:]
+        for c in a:
+            residue = residue.replace(c, '', 1)
+
+        # append leftover to the end of the alignment
+        alignments[i] += residue
+
+        # keep track of longest alignment length
+        length = len(alignments[i])
+        if length > maxlen:
+            maxlen = length
+
+    # pad ends of alignments if they are not maxlen length
+    for i, a in enumerate(alignments):
+        dif = maxlen - len(a)
+        for j in range(dif):
+            alignments[i] += "."
+
+    return alignments
+
+
+def MSA(sequences, global_align=True):
     seqs = ["." + s for s in sequences]
     dimensions = [len(s) for s in seqs]
 
     matrix = NMatrix(dimensions)
     best_cell = (-inf, None)
 
+    # origin = 0
     cur_pos = [0] * len(dimensions)
     matrix[cur_pos] = (0, [])
-    #next pos
+    # next pos
     cur_pos[-1] = 1
 
     final_pos = [d-1 for d in dimensions]
-    #final_pos = [0,0,1]
-
     nMasks = neighbourMask(len(dimensions))
 
-    print(f'Dimensions: {dimensions}\n')
+    print(f'Filling matrix of dimensions: {dimensions}\n')
 
-    #TODO CLEAN CODE
-    #TODO BETTER SUM OF PAIRS
-
+    # Fill each cell in matrix in order
     while(True):
+        # neighbouring cells to consider and a place to store their scores
         neighbours = [vectorSum(cur_pos, mask) for mask in nMasks]
         scores = [-inf]*len(neighbours)
 
+        # parse scores from each neighbour (if exists)
         for i, nPos in enumerate(neighbours):
             nVal = matrix[nPos]
             if nVal is None:
                 continue
+            scores[i] = parseNeighbour(cur_pos, seqs, nVal[0], nMasks[i])
 
-            scores[i] = nVal[0]
-
-            mask = nMasks[i]
-            gap_positions = []
-            match_positions = []
-            for j, v in enumerate(mask):
-                gap_positions.append(j) if v == 0 else match_positions.append(j)
-
-            scores[i] += len(gap_positions)*INDEL if len(gap_positions) < MULTI_GAP_THRESHOLD else MULTI_GAP
-
-            if len(match_positions) > 1:
-                combos = list(combinations(match_positions, 2))
-                for combo in combos:
-                    seqA = seqs[combo[0]]
-                    seqB = seqs[combo[1]]
-                    a = seqA[cur_pos[combo[0]]]
-                    b = seqB[cur_pos[combo[1]]]
-                    scores[i] += MATCH if a == b else MISMATCH
-
+        # get max score and the neighbour mask that got us there
         maxScore = max(scores)
         maxI = scores.index(maxScore)
         delta = nMasks[maxI]
 
+        # current cel = (score, parent coordinate mask)
         matrix[cur_pos] = (maxScore, delta)
 
+        # keep track of best cell
         if maxScore > best_cell[0]:
             best_cell = (maxScore, cur_pos)
 
+        # stop if matrix filled, else next cel position
         if cur_pos == final_pos:
             break
         cur_pos = nextMatrixPosition(cur_pos, dimensions)
 
-    print(f'Alignment score: {matrix[final_pos][0]}')
+    if global_align:
+        print(f'Final alignment score: {matrix[final_pos][0]}\n')
+        path = globalAlignmentPath(matrix)
 
-    path = []
-    cur_pos = final_pos
-    while(True):
-        if cur_pos == [0] * len(dimensions):
-            break
-        cell = matrix[cur_pos]
-        path.append((cell[1], cur_pos))
-        cur_pos = vectorSum(cur_pos, cell[1])
-    path.reverse()
+        return parseGlobalAlignments(seqs, path)
 
-    #print(path)
-    alignments = ["" for s in seqs]
-    for node in path:
-        delta = node[0]
-        for pos, coord in enumerate(delta):
-            alignments[pos] += "." if coord == 0 else seqs[coord][node[1][coord]]
-
-    maxlen = 0
-    for i, a in enumerate(alignments):
-        residue = seqs[i]
-        for c in a:
-            residue = residue.replace(c, '', 1)
-        alignments[i] += residue
-        length = len(alignments[i])
-        if length > maxlen:
-            maxlen = length
-
-    for i, a in enumerate(alignments):
-        dif = maxlen - len(a)
-        for j in range(dif):
-            alignments[i] += "."
-
-
-    for i, a in enumerate(alignments):
-        print(f's{i+1}: {a}')
 
 def main():
     sequences = [
-        "A",
-        "C",
-        "C"
+        "CAG",
+        "ACT",
     ]
-    MSA(sequences)
+    alignments = MSA(sequences)
+
+    for i, a in enumerate(alignments):
+        print(f's{i+1}: {a}')
 
 if __name__ == '__main__':
     main()
